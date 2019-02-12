4e7e7dcd0aae39c59f67e156e9f32c779706245e
victoriak94 <vkamila6@gmail.com>
2019-02-11 22:53:42 -0500

    Use DOMContentLoaded

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,21 @@
 
-   let filterInput = document.querySelector("#filterInput");
-   filterInput.addEventListener("keyup", filterNames);
-
-   function filterNames(){
-     let filterValue = document.querySelector("#filterInput").value.toUpperCase();
-     let ul = document.querySelector("#names");
-     let li = ul.querySelectorAll("li.name-item");
-
-     for(let i = 0; i < li.length; i++){
-       let a = li[i].getElementsByTagName('a')[0];
-       if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
-         li[i].style.display = '';
-       } else {
-         li[i].style.display = 'none';
-       }
-     }
-
-   }
+document.addEventListener('DOMContentLoaded', function(e) {
+  let filterInput = document.querySelector("#filterInput");
+  filterInput.addEventListener("keyup", filterNames);
+ 
+  function filterNames(){
+    let filterValue = document.querySelector("#filterInput").value.toUpperCase();
+    let ul = document.querySelector("#names");
+    let li = ul.querySelectorAll("li.name-item");
+ 
+    for(let i = 0; i < li.length; i++){
+      let a = li[i].getElementsByTagName('a')[0];
+      if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+        li[i].style.display = '';
+      } else {
+        li[i].style.display = 'none';
+      }
+    }
+ 
+  }
+});
